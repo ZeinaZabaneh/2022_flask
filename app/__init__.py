74fb44d1d994df15faa9c8ceb9285b79081dcb5f
ZeinaZabaneh <10476052@uwa.student.edu.au>
2022-05-20 22:58:48 +0800

    start of db

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 import os
 
-from flask import Flask 
-from flask import Config
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
-
-from app import routes 
+from app import routes, models 
