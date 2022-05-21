fa3bfdf863fb5cde91bd62f045d925cb67e9ad59
ZeinaZabaneh <10476052@uwa.student.edu.au>
2022-05-21 13:04:36 +0800

    beginning of db creation

--- app/models.py
+++ app/models.py
@@ -2,9 +2,9 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    user_IPAdress = db.Column(db.String(64), index=True, unique=True)
+    date = db.Column(db.DateTime, index=True, default=datetime.now)
+    score = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<Score {}>'.format(self.score)
